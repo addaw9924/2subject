617c6dd756adfc9a15fd03687510253a451499b5
Mac_1 <Mac_1@DESKTOP-P3QHQ70>
2021-08-30 17:06:58 +0900

    1234

--- First/src/hello.java
+++ First/src/hello.java
@@ -2,5 +2,6 @@ public class hello {
 	public static void main(String[] args) {
 		System.out.println("hello java word!");
 		System.out.println("hi!");
+		System.out.println("mr.yesterday");
 	}
 }
---
+++ First/src/twelve.java
@@ -0,0 +1,9 @@
+
+public class twelve {
+
+	public static void main(String[] args) {
+		System.out.println("testing");
+
+	}
+
+}
